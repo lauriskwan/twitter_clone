9dd84ec4e1db172041b19275d04d12f363a231ee
Lauris Kwan <kwanlauris@gmail.com>
2023-03-08 00:10:09 +0800

    post of feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,29 @@
 import { SparklesIcon } from "@heroicons/react/outline";
 import Input from "./Input";
+import Post from "./Post";
 
 export default function Feed() {
+  // Dummy Data
+  const posts = [
+    {
+      id: "1",
+      name: "Test User",
+      username: "testusername",
+      userImg: "https://cdn-icons-png.flaticon.com/512/147/147144.png",
+      img: "https://cdn.cms-twdigitalassets.com/content/dam/blog-twitter/official/en_us/products/2022/recommendations-on-twitter/how-recommendations-help-discover-more-twitter-1.jpg.img.fullhd.medium.jpg",
+      text: "UI demo",
+      timestamp: "2 hours ago",
+    },
+    {
+      id: "2",
+      name: "Test User",
+      username: "testusername",
+      userImg: "https://cdn-icons-png.flaticon.com/512/147/147144.png",
+      img: "https://poet.so/cover-photo.png",
+      text: "another UI demo",
+      timestamp: "3 hours ago",
+    },
+  ];
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 sm:ml-[73px] flex-grow max-w-xl ">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -11,6 +33,9 @@ export default function Feed() {
         </div>
       </div>
       <Input />
+      {posts.map((post) => {
+        return <Post key={post.id} post={post} />;
+      })}
     </div>
   );
 }
