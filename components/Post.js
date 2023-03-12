3b60181fadc0d2711051c7333b6f73e4a1902ad4
Lauris Kwan <kwanlauris@gmail.com>
2023-03-12 18:33:21 +0800

    delete post function

--- components/Post.js
+++ components/Post.js
@@ -17,7 +17,9 @@ import {
 import Moment from "react-moment";
 import { signIn, useSession } from "next-auth/react"; // help getting user.id
 import { useEffect, useState } from "react";
-import { db } from "@/firebase";
+import { db, storage } from "@/firebase";
+import { async } from "@firebase/util";
+import { deleteObject, ref } from "firebase/storage";
 
 export default function Post({ post }) {
   const { data: session } = useSession();
@@ -49,6 +51,14 @@ export default function Post({ post }) {
       }
     } else {
       signIn();
+    }
+  }
+
+  async function deletePost() {
+    if (window.confirm("Are you sure you want to delete this post?")) {
+      // generating a modal automatically
+      deleteDoc(doc(db, "posts", post.id)); // delete text elements
+      deleteObject(ref(storage, `posts/${post.id}/image`)); // delete media
     }
   }
 
@@ -98,7 +108,13 @@ export default function Post({ post }) {
         {/* utility icons */}
         <div className="flex justify-between text-gray-500 p-2">
           <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
-          <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+          {session?.user.uid === post?.data().id && (
+            <TrashIcon
+              onClick={deletePost}
+              className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
+            />
+          )}
+
           <div className="flex items-center">
             {hasLiked ? (
               <HeartIconFilled
